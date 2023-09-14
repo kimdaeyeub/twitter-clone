bed8185526440775878bb114ae240c522e9b4d00
kimdaeyeub <kdy3915@naver.com>
2023-09-14 22:22:48 +0900

    Add provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import 'package:twitter/features/common/main_navigation_screen.dart';
 import 'package:twitter/router.dart';
 
@@ -12,23 +13,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          systemOverlayStyle: SystemUiOverlayStyle.dark,
-          backgroundColor: Colors.transparent,
-          foregroundColor: Colors.black,
-          elevation: 0,
-        ),
-      ),
-      darkTheme: ThemeData(
-          scaffoldBackgroundColor: Colors.black,
-          textTheme: Typography.whiteCupertino,
+    return Provider(
+      create: (context) => {},
+      child: MaterialApp.router(
+        routerConfig: router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
           appBarTheme: const AppBarTheme(
+            systemOverlayStyle: SystemUiOverlayStyle.dark,
             backgroundColor: Colors.transparent,
-          )),
+            foregroundColor: Colors.black,
+            elevation: 0,
+          ),
+        ),
+        darkTheme: ThemeData(
+            scaffoldBackgroundColor: Colors.black,
+            textTheme: Typography.whiteCupertino,
+            appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.transparent,
+            )),
+      ),
     );
   }
 }
